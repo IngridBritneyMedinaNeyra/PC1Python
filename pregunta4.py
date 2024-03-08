32d46d7fec5a0131538c7dc00b88c62c8c7a0f35
IngridBritneyMedinaNeyra <162529460+IngridBritneyMedinaNeyra@users.noreply.github.com>
2024-03-08 01:47:37 +0000

    solucionpregunta4

---
+++ pregunta4.py
@@ -0,0 +1,8 @@
+#1 Solicitamos al usuario un entero positivo N
+N = int(input("Ingrese un entero positivo N: "))
+
+#2 Calculamos la suma utilizando la fórmula
+suma = (N * (N + 1)) // 2
+
+#3 Mostramos la suma en la pantalla
+print(f"La suma de los enteros desde 1 hasta {N} es: {suma}")
