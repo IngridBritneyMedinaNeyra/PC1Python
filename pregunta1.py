02c66b133e2f0d1f00a29dde051b9b46207dd0b8
IngridBritneyMedinaNeyra <162529460+IngridBritneyMedinaNeyra@users.noreply.github.com>
2024-03-08 00:46:35 +0000

    solucionpregunta1

---
+++ pregunta1.py
@@ -0,0 +1,5 @@
+#1 Solicitamos el nombre de usuario por consola
+nombre_usuario = input("Introduce tu nombre de usuario: ")
+
+#2 Mostramos el saludo con el nombre del usuario
+print("¡Hola", nombre_usuario + "!")
