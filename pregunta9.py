46f3e5f06ad9d65a9ce31523f749ead72e43e19e
IngridBritneyMedinaNeyra <162529460+IngridBritneyMedinaNeyra@users.noreply.github.com>
2024-03-08 02:41:13 +0000

    solucionpregunta9

---
+++ pregunta9.py
@@ -0,0 +1,9 @@
+#1 Defininamos la lista original
+lista_original = ['Di', 'buen', 'día', 'a', 'papa']
+
+#2 Invertimos la lista
+lista_invertida = lista_original[::-1]
+
+#3 Mostramos la lista invertida
+print("Lista Original:", lista_original)
+print("Lista Invertida:", lista_invertida)
